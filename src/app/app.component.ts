85bf23341471a07e9587aa4c2e9662890d4efa5c
Tyler Crumpton <tyler.crumpton@gmail.com>
2017-04-09 15:31:11 -0500

    Handle blank and invalid REs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,43 +7,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   matchWords = [
-    {word: 'dir', matched: true},
-    {word: 'chkdsk', matched: true},
-    {word: 'diskpart', matched: true},
-    {word: 'run', matched: true},
-    {word: 'md', matched: true},
-    {word: 'ipconfig', matched: true},
-    {word: 'mountvol', matched: true},
-    {word: 'nslookup', matched: true},
-    {word: 'perfmon', matched: true},
-    {word: 'robocopy', matched: true},
-    {word: 'taskkill', matched: true},
+    {word: 'dir', matched: false},
+    {word: 'chkdsk', matched: false},
+    {word: 'diskpart', matched: false},
+    {word: 'run', matched: false},
+    {word: 'md', matched: false},
+    {word: 'ipconfig', matched: false},
+    {word: 'mountvol', matched: false},
+    {word: 'nslookup', matched: false},
+    {word: 'perfmon', matched: false},
+    {word: 'robocopy', matched: false},
+    {word: 'taskkill', matched: false},
   ];
   missWords = [
-    {word: 'sudo', matched: true},
-    {word: 'apt-get', matched: true},
-    {word: 'ifconfig', matched: true},
-    {word: 'startx', matched: true},
-    {word: 'killall', matched: true},
-    {word: 'man', matched: true},
-    {word: 'tar xzf', matched: true},
-    {word: 'cat', matched: true},
-    {word: 'tail', matched: true},
-    {word: 'whoami', matched: true},
-    {word: 'finger', matched: true},
+    {word: 'sudo', matched: false},
+    {word: 'apt-get', matched: false},
+    {word: 'ifconfig', matched: false},
+    {word: 'startx', matched: false},
+    {word: 'killall', matched: false},
+    {word: 'man', matched: false},
+    {word: 'tar xzf', matched: false},
+    {word: 'cat', matched: false},
+    {word: 'tail', matched: false},
+    {word: 'whoami', matched: false},
+    {word: 'finger', matched: false},
   ];
   regexStringModel = "";
+  badRegex = false;
 
   checkWords() {
     let self = this;
+    if (self.regexStringModel.length === 0) {
+      self.badRegex = false;
+      this.unMatchAll();
+    }
+    else {
+      try {
+        this.matchWords = this.matchWords.map((word) => {
+          let matches = new RegExp(self.regexStringModel).test(word.word);
+          return {word: word.word, matched: matches};
+        });
+        this.missWords = this.missWords.map((word) => {
+          let matches = new RegExp(self.regexStringModel).test(word.word);
+          return {word: word.word, matched: matches};
+        });
+        self.badRegex = false;
+      } catch (Error) {
+        self.badRegex = true;
+        this.unMatchAll();
+      }
+    }
+  }
+
+  unMatchAll() {
     this.matchWords = this.matchWords.map((word) => {
-      let matches = new RegExp(self.regexStringModel).test(word.word);
-      return {word: word.word, matched: matches};
+      return {word: word.word, matched: false};
     });
     this.missWords = this.missWords.map((word) => {
-      let matches = new RegExp(self.regexStringModel).test(word.word);
-      return {word: word.word, matched: matches};
+      return {word: word.word, matched: false};
     });
-
   }
 }
