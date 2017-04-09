51d2e90b249fc294f88edba924d3323a5cc4e1cb
Tyler Crumpton <tyler.crumpton@gmail.com>
2017-04-09 02:38:23 -0500

    Add initial pass at golfer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rg works!';
+  matchWords = [
+    {word: 'dir', matched: true},
+    {word: 'chkdsk', matched: true},
+    {word: 'diskpart', matched: true},
+    {word: 'run', matched: true},
+    {word: 'md', matched: true},
+    {word: 'ipconfig', matched: true},
+    {word: 'mountvol', matched: true},
+    {word: 'nslookup', matched: true},
+    {word: 'perfmon', matched: true},
+    {word: 'robocopy', matched: true},
+    {word: 'taskkill', matched: true},
+  ];
+  missWords = [
+    {word: 'sudo', matched: true},
+    {word: 'apt-get', matched: true},
+    {word: 'ifconfig', matched: true},
+    {word: 'startx', matched: true},
+    {word: 'killall', matched: true},
+    {word: 'man', matched: true},
+    {word: 'tar xzf', matched: true},
+    {word: 'cat', matched: true},
+    {word: 'tail', matched: true},
+    {word: 'whoami', matched: true},
+    {word: 'finger', matched: true},
+  ];
+  regexStringModel = "";
+
+  checkWords() {
+    let self = this;
+    this.matchWords = this.matchWords.map((word) => {
+      let matches = new RegExp(self.regexStringModel).test(word.word);
+      return {word: word.word, matched: matches};
+    });
+    this.missWords = this.missWords.map((word) => {
+      let matches = new RegExp(self.regexStringModel).test(word.word);
+      return {word: word.word, matched: matches};
+    });
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
